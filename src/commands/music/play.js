b125e9d1ebeabe11e599ddbf16ea2b52e6c3f49e
LogGame-HU-DEaN <35978189+LogGame-HU-DEaN@users.noreply.github.com>
2018-06-03 09:32:57 +0200

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -73,7 +73,11 @@ module.exports = class Play extends Command {
     message.channel.send(
       new SwitchbladeEmbed()
         .setThumbnail(playlist.artwork)
-        .setDescription(`${Constants.PLAY_BUTTON} **${amount} songs from playlist** [${playlist.title}](${playlist.uri})${duration} **has been added to queue!**`)
+        if($(amount) == 1) {
+          .setDescription(`${Constants.PLAY_BUTTON} **1 song from playlist** [${playlist.title}](${playlist.uri})${duration} **has been added to queue!**`)
+        } else {
+          .setDescription(`${Constants.PLAY_BUTTON} **${amount} songs from playlist** [${playlist.title}](${playlist.uri})${duration} **have been added to queue!**`)
+        }
     )
   }
 
